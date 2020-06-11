2fb53dd929e82b9e6e4a1bb371fce9a86d2a9027
Ake Hedman <akhe@grodansparadis.com>
2020-06-11 17:01:06 +0200

    Added XML to JSON file converter

---
+++ script/xml2json.py
@@ -0,0 +1,72 @@
+#
+# VSCP (Very Simple Control Protocol)
+# http://www.vscp.org
+#
+# The MIT License (MIT)
+#
+# Copyright (C) 2000-2020 Ake Hedman, 
+# Grodans Paradis AB <info@grodansparadis.com>
+#
+# Convert XML file to JSON  xml2json filename.xml >filename.json
+#
+
+import sys
+import os
+import getopt
+import glob
+import xmltodict, json
+
+def usage():
+    print("usage: xml2json.py inputfile.xml >outputfile.json ")
+    print("---------------------------------------------")
+    print("-h/--help    - This text.")
+    print("-v/--verbose - Print output also to screen.")
+#    print("-o/--outdir  - Folder to write output files to.")
+#    print("               defaults to current folder.")
+
+path = '../classes/class_10_measurement.xml'
+data = ''
+
+args = sys.argv[1:]
+nargs = len(args)
+
+outdir = "./"
+bverbose = False
+
+try:
+    opts, args = getopt.getopt(args,"hvo:",["help","verbose","outdir="])
+except getopt.GetoptError:
+    print("unrecognized format!")
+    usage()
+    sys.exit(2)
+for opt, arg in opts:
+    if opt in ("-h", "--help"):
+        print("HELP")
+        usage()
+        sys.exit()
+    elif opt in ("-v", "--verbose"):
+        bverbose = True
+    elif opt in ("-o", "--outdir"):
+        outdir = arg
+        os.makedirs(outdir, exist_ok=True)
+
+# Read input file
+types = open(path,'r')
+indata =  types.read()
+types.close()
+
+if bverbose:
+    print( "Inputdata\n", indata  )
+    print('--------------------------------------------------------------------------')
+
+o = xmltodict.parse(indata)
+outdata = json.dumps(o)
+
+if bverbose:
+    print( outdata )
+    print( o['class']['@token'])
+
+# Write the file
+#file = open(outdir + "/" + filename,"w")
+#file.write(outstr)
+#file.close
